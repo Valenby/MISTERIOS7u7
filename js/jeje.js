6a839ed85fd2ee31095fd5f3c97f7ca1d88b196d
valenby <30valep@gmail.com>
2022-05-31 01:03:42 +0200

    archivos faltantes

---
+++ js/jeje.js
@@ -0,0 +1,4 @@
+main () {} // funcion principal.
+// programa secuencia de l9neas con secuencia
+/* jdsakhdausjdhasldnalskdalksdjadkal */ // documentar
+// estandar para imprimir por pantalla (namespace)
